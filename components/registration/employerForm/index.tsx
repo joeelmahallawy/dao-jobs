963c9eddaff1c66dcf5898a3d86ae00393ab6828
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2021-12-28 01:09:22 -0500

    added form validation for employer registration form

--- components/registration/employerForm/index.tsx
+++ components/registration/employerForm/index.tsx
@@ -2,6 +2,7 @@ import {
   Box,
   Button,
   Center,
+  FormControl,
   FormLabel,
   Heading,
   Input,
@@ -9,6 +10,7 @@ import {
   RadioGroup,
   Stack,
   Textarea,
+  Text,
 } from "@chakra-ui/react";
 import { Field, Formik, Form } from "formik";
 import React from "react";
@@ -24,7 +26,6 @@ type InitialValues = {
   briefDescription: string;
 };
 const EmployerForm = ({ user }) => {
-  console.log(user);
   const initValues: InitialValues = {
     nameOfDao: "",
     discordServerExists: "",
@@ -48,17 +49,17 @@ const EmployerForm = ({ user }) => {
         initialValues={initValues}
         validate={(values) => {
           const errors = {};
-          // if (!values.email) {
-          //   errors.email = "Required";
-          // } else if (
-          //   !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          // ) {
-          //   errors.email = "Invalid email address";
-          // }
+          // TODO:TODO:TODO:TODO:
+          Object.entries(values).forEach((val) => {
+            if (!values[val[0]]) errors[val[0]] = "Required";
+          });
+          if (!/^(ftp|http|https):\/\/[^ "]+$/.test(values.twitterUrl))
+            errors["twitterUrl"] = "Invalid URL";
           return errors;
         }}
         onSubmit={(values, { setSubmitting }) => {
           console.log(values);
+          console.log("OHYA BABY WE GOT IT WORKING");
         }}
       >
         {({
@@ -72,97 +73,114 @@ const EmployerForm = ({ user }) => {
           setFieldValue,
         }) => (
           <Form onSubmit={handleSubmit}>
-            <FormLabel>Enter the name of your dao</FormLabel>
-            <Input
-              placeholder="Ex. Olympus DAO"
-              outline="1px solid gray"
-              _focus={{ bg: "white" }}
-              name="nameOfDao"
-              type="text"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.nameOfDao}
-            />
+            <FormControl isRequired>
+              <FormLabel>Enter the name of your dao</FormLabel>
+              <Input
+                placeholder="Ex. Olympus DAO"
+                outline="1px solid gray"
+                _focus={{ bg: "white" }}
+                name="nameOfDao"
+                type="text"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.nameOfDao}
+              />
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel mt="1.5rem">
+                Does your DAO have a discord server yet?
+              </FormLabel>
+              <RadioGroup
+                value={values.discordServerExists}
+                onChange={(event: string) => {
+                  setFieldValue("discordServerExists", event);
+                }}
+              >
+                <Stack spacing={5} direction="row">
+                  <Radio value="yes">Yes</Radio>
+                  <Radio value="no">No</Radio>
+                </Stack>
+              </RadioGroup>
+            </FormControl>
 
-            <FormLabel mt="1.5rem">
-              Does your DAO have a discord server yet?
-            </FormLabel>
-            <RadioGroup
-              value={values.discordServerExists}
-              onChange={(event: string) => {
-                setFieldValue("discordServerExists", event);
-              }}
-            >
-              <Stack spacing={5} direction="row">
-                <Radio value="yes">Yes</Radio>
-                <Radio value="no">No</Radio>
-              </Stack>
-            </RadioGroup>
+            <FormControl isRequired>
+              <FormLabel mt="1.5rem">
+                If answered yes above, please enter the discord link. Otherwise,
+                write 'N/A'
+              </FormLabel>
+              <Input
+                outline="1px solid gray"
+                _focus={{ bg: "white" }}
+                placeholder="Ex. https://discord.gg/12345"
+                type="text"
+                name="discordLink"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.discordLink}
+              />
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel mt="1.5rem">
+                Please enter approximate number of members in the discord server
+                and 'N/A' otherwise
+              </FormLabel>
+              <Input
+                outline="1px solid gray"
+                _focus={{ bg: "white" }}
+                placeholder="Ex. 1000"
+                type="text"
+                name="discordPopulation"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.discordPopulation}
+              />
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel mt="1.5rem">
+                Please enter Twitter URL and 'N/A' otherwise
+              </FormLabel>
+              <Input
+                outline="1px solid gray"
+                _focus={{ bg: "white" }}
+                placeholder="Ex. https://twitter.com/OlympusDAO"
+                type="text"
+                name="twitterUrl"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.twitterUrl}
+              />
+              {touched.twitterUrl && (
+                <Text color="red">{errors.twitterUrl}</Text>
+              )}
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel mt="1.5rem">What is the goal of your DAO?</FormLabel>
 
-            <FormLabel mt="1.5rem">
-              If answered yes above, please enter the discord link. Otherwise,
-              write 'N/A'
-            </FormLabel>
-            <Input
-              outline="1px solid gray"
-              _focus={{ bg: "white" }}
-              placeholder="Ex. https://discord.gg/12345"
-              type="text"
-              name="discordLink"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.discordLink}
-            />
-
-            <FormLabel mt="1.5rem">
-              Approximately how many people are in the discord server?
-            </FormLabel>
-            <Input
-              outline="1px solid gray"
-              _focus={{ bg: "white" }}
-              placeholder="Ex. 1000"
-              type="text"
-              name="discordPopulation"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.discordPopulation}
-            />
-            <FormLabel mt="1.5rem">
-              Please enter Twitter URL and 'N/A' otherwise
-            </FormLabel>
-            <Input
-              outline="1px solid gray"
-              _focus={{ bg: "white" }}
-              placeholder="Ex. https://twitter.com/OlympusDAO"
-              type="text"
-              name="twitterUrl"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.twitterUrl}
-            />
-            <FormLabel mt="1.5rem">What is the goal of your DAO?</FormLabel>
-            <Textarea
-              outline="1px solid gray"
-              _focus={{ bg: "white" }}
-              type="text"
-              name="daoGoals"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.daoGoals}
-            />
-            <FormLabel mt="1.5rem">
-              Give a brief general description of your DAO for job seekers to
-              read{" "}
-            </FormLabel>
-            <Textarea
-              outline="1px solid gray"
-              _focus={{ bg: "white" }}
-              type="text"
-              name="briefDescription"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.briefDescription}
-            />
+              <Textarea
+                outline="1px solid gray"
+                _focus={{ bg: "white" }}
+                type="text"
+                name="daoGoals"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.daoGoals}
+              />
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel mt="1.5rem">
+                Give a brief general description of your DAO for job seekers to
+                read{" "}
+              </FormLabel>
+              <Textarea
+                outline="1px solid gray"
+                _focus={{ bg: "white" }}
+                type="text"
+                name="briefDescription"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.briefDescription}
+              />
+            </FormControl>
 
             <Button
               mt="1rem"
