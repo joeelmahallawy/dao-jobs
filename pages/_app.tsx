b782ee87db8a8e0f05963196c10abc7595da55e4
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2022-01-05 20:24:06 -0500

    configured GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,85 +17,110 @@ import ReactGA from 'react-ga'
 import App from 'next/app'
 import { useRouter } from 'next/router'
 
-const MyApp = ({ Component, pageProps: { session, ...pageProps } }) => {
-    const [authenticatedState, setAuthenticatedState] =
-        useState('not-authenticated')
-    useEffect(() => {
+class MyApp extends App {
+    async componentDidMount() {
         if (process.env.NODE_ENV === 'production') {
             ReactGA.initialize('UA-216449673-1')
-            logPageView()
-        }
-
-        function logPageView() {
-            const router = useRouter()
-
-            if (process.env.NODE_ENV === 'production') {
-                ReactGA.pageview(router.pathname)
-            }
-        }
-
-        const { data: authListener } = supabase.auth.onAuthStateChange(
-            (event, session) => {
-                const user = supabase.auth.user()
-                if (user) {
-                    handleAuthChange(event, session)
+            this.logPageView()
+            const [authenticatedState, setAuthenticatedState] =
+                useState('not-authenticated')
+            useEffect(() => {
+                if (process.env.NODE_ENV === 'production') {
+                    ReactGA.initialize('UA-216449673-1')
+                    logPageView()
                 }
-                console.log('event and session', event, session)
 
-                if (event === 'SIGNED_IN') {
+                function logPageView() {
+                    const router = useRouter()
+
+                    if (process.env.NODE_ENV === 'production') {
+                        ReactGA.pageview(router.pathname)
+                    }
+                }
+
+                const { data: authListener } = supabase.auth.onAuthStateChange(
+                    (event, session) => {
+                        const user = supabase.auth.user()
+                        if (user) {
+                            handleAuthChange(event, session)
+                        }
+                        console.log('event and session', event, session)
+
+                        if (event === 'SIGNED_IN') {
+                            setAuthenticatedState('authenticated')
+                        }
+
+                        if (event === 'SIGNED_OUT') {
+                            setAuthenticatedState('not-authenticated')
+                        }
+                    },
+                )
+                checkUser()
+                return () => {
+                    authListener.unsubscribe()
+                }
+            }, [])
+            const checkUser = async () => {
+                const user = await supabase.auth.user()
+                if (user) {
                     setAuthenticatedState('authenticated')
                 }
+            }
+            process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT
 
-                if (event === 'SIGNED_OUT') {
-                    setAuthenticatedState('not-authenticated')
-                }
-            },
+            const handleAuthChange = async (event, session) => {
+                await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auth`, {
+                    method: 'POST',
+                    headers: new Headers({
+                        'Content-Type': 'application/json',
+                        cookie: session.refresh_token,
+                    }),
+                    credentials: 'same-origin',
+                    body: JSON.stringify({
+                        event,
+                        session,
+                    }),
+                })
+            }
+        }
+    }
+
+    componentDidUpdate() {
+        this.logPageView()
+    }
+
+    logPageView() {
+        const { router } = this.props
+
+        if (process.env.NODE_ENV === 'production') {
+            ReactGA.pageview(router.asPath)
+        }
+    }
+
+    render() {
+        const { Component, pageProps } = this.props
+
+        return (
+            <>
+                <RecoilRoot>
+                    <ChakraProvider theme={Chakratheme}>
+                        <DefaultSeo {...createSEOConfig()} />
+                        <Head>
+                            <link rel="icon" href={icon.src} />
+                            <title>Anura DAO</title>
+                            <meta
+                                name="viewport"
+                                content="initial-scale=1, width=device-width"
+                            />
+                        </Head>
+                        <Layout>
+                            <Component {...pageProps} />
+                        </Layout>
+                    </ChakraProvider>
+                </RecoilRoot>
+            </>
         )
-        checkUser()
-        return () => {
-            authListener.unsubscribe()
-        }
-    }, [])
-    const checkUser = async () => {
-        const user = await supabase.auth.user()
-        if (user) {
-            setAuthenticatedState('authenticated')
-        }
     }
-    process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT
-
-    const handleAuthChange = async (event, session) => {
-        await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auth`, {
-            method: 'POST',
-            headers: new Headers({
-                'Content-Type': 'application/json',
-                cookie: session.refresh_token,
-            }),
-            credentials: 'same-origin',
-            body: JSON.stringify({
-                event,
-                session,
-            }),
-        })
-    }
-
-    return (
-        <RecoilRoot>
-            <ChakraProvider theme={Chakratheme}>
-                <DefaultSeo {...createSEOConfig()} />
-                <Head>
-                    <link rel="icon" href={icon.src} />
-                    <title>Anura DAO</title>
-                    <meta
-                        name="viewport"
-                        content="initial-scale=1, width=device-width"
-                    />
-                </Head>
-                <Layout>
-                    <Component {...pageProps} />
-                </Layout>
-            </ChakraProvider>
-        </RecoilRoot>
-    )
 }
+
 export default MyApp
