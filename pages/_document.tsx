773b8865d96bce5c9acc3ff707381f8021277817
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2022-01-05 20:52:17 -0500

    correctly caught data on _documnet

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,28 +1,15 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react'
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
+
     render() {
         return (
             <Html>
-                <Head>
-                    {/* Global Site Tag (gtag.js) - Google Analytics */}
-                    <script
-                        async
-                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-                    />
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-                        }}
-                    />
-                </Head>
                 <body>
                     <Main />
                     <NextScript />
@@ -31,3 +18,5 @@ export default class MyDocument extends Document {
         )
     }
 }
+
+export default MyDocument
