001244b8eb8942820eb7b7c42b0acdedf2bd4989
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2022-01-05 20:36:26 -0500

    configured GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,114 +13,89 @@ import { DefaultSeo } from 'next-seo'
 import { createSEOConfig } from '../utils/seoMeta'
 import Head from 'next/head'
 import icon from '../attachments/daojobs-icon.ico'
-import ReactGA from 'react-ga'
-import App from 'next/app'
 import { useRouter } from 'next/router'
+import ReactGA from 'react-ga'
 
-class MyApp extends App {
-    async componentDidMount() {
-        if (process.env.NODE_ENV === 'production') {
-            ReactGA.initialize('UA-216449673-1')
-            this.logPageView()
-            const [authenticatedState, setAuthenticatedState] =
-                useState('not-authenticated')
-            useEffect(() => {
-                if (process.env.NODE_ENV === 'production') {
-                    ReactGA.initialize('UA-216449673-1')
-                    logPageView()
-                }
-
-                function logPageView() {
-                    const router = useRouter()
-
-                    if (process.env.NODE_ENV === 'production') {
-                        ReactGA.pageview(router.pathname)
-                    }
-                }
-
-                const { data: authListener } = supabase.auth.onAuthStateChange(
-                    (event, session) => {
-                        const user = supabase.auth.user()
-                        if (user) {
-                            handleAuthChange(event, session)
-                        }
-                        console.log('event and session', event, session)
-
-                        if (event === 'SIGNED_IN') {
-                            setAuthenticatedState('authenticated')
-                        }
-
-                        if (event === 'SIGNED_OUT') {
-                            setAuthenticatedState('not-authenticated')
-                        }
-                    },
-                )
-                checkUser()
-                return () => {
-                    authListener.unsubscribe()
-                }
-            }, [])
-            const checkUser = async () => {
-                const user = await supabase.auth.user()
-                if (user) {
-                    setAuthenticatedState('authenticated')
-                }
-            }
-            process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT
-
-            const handleAuthChange = async (event, session) => {
-                await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auth`, {
-                    method: 'POST',
-                    headers: new Headers({
-                        'Content-Type': 'application/json',
-                        cookie: session.refresh_token,
-                    }),
-                    credentials: 'same-origin',
-                    body: JSON.stringify({
-                        event,
-                        session,
-                    }),
-                })
-            }
-        }
-    }
-
-    componentDidUpdate() {
-        this.logPageView()
-    }
-
-    logPageView() {
-        const { router } = this.props
+const MyApp = ({ Component, pageProps: { session, ...pageProps } }) => {
+    const logPageView = () => {
+        const router = useRouter()
 
         if (process.env.NODE_ENV === 'production') {
             ReactGA.pageview(router.asPath)
         }
     }
 
-    render() {
-        const { Component, pageProps } = this.props
+    const [authenticatedState, setAuthenticatedState] =
+        useState('not-authenticated')
+    useEffect(() => {
+        if (process.env.NODE_ENV === 'production') {
+            ReactGA.initialize('UA-216449673-1')
+            logPageView()
+        }
 
-        return (
-            <>
-                <RecoilRoot>
-                    <ChakraProvider theme={Chakratheme}>
-                        <DefaultSeo {...createSEOConfig()} />
-                        <Head>
-                            <link rel="icon" href={icon.src} />
-                            <title>Anura DAO</title>
-                            <meta
-                                name="viewport"
-                                content="initial-scale=1, width=device-width"
-                            />
-                        </Head>
-                        <Layout>
-                            <Component {...pageProps} />
-                        </Layout>
-                    </ChakraProvider>
-                </RecoilRoot>
-            </>
+        const { data: authListener } = supabase.auth.onAuthStateChange(
+            (event, session) => {
+                const user = supabase.auth.user()
+                if (user) {
+                    handleAuthChange(event, session)
+                }
+                console.log('event and session', event, session)
+
+                if (event === 'SIGNED_IN') {
+                    setAuthenticatedState('authenticated')
+                }
+
+                if (event === 'SIGNED_OUT') {
+                    setAuthenticatedState('not-authenticated')
+                }
+            },
         )
-    }
-}
+        checkUser()
+        return () => {
+            authListener.unsubscribe()
+        }
+    }, [])
 
+    const checkUser = async () => {
+        const user = await supabase.auth.user()
+        if (user) {
+            setAuthenticatedState('authenticated')
+        }
+    }
+    process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT
+
+    const handleAuthChange = async (event, session) => {
+        await fetch(process.env.NEXT_PUBLIC_BASE_URL + '/api/auth', {
+            method: 'POST',
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                cookie: session.refresh_token,
+            }),
+            credentials: 'same-origin',
+            body: JSON.stringify({
+                event,
+                session,
+            }),
+        })
+    }
+
+    return (
+        <RecoilRoot>
+            <ChakraProvider theme={Chakratheme}>
+                <DefaultSeo {...createSEOConfig()} />
+                <Head>
+                    <link rel="icon" href={icon.src} />
+                    <title>Anura DAO</title>
+                    <meta
+                        name="viewport"
+                        content="initial-scale=1, width=device-width"
+                    />
+                </Head>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </ChakraProvider>
+        </RecoilRoot>
+    )
+}
 export default MyApp
